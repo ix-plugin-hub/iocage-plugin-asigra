1abb472c4974baff3601a1a1a3828faa25ac5ee3
John Hixson <john@ixsystems.com>
2018-07-25 17:11:55 -0400

    bleh

--- post_install.sh
+++ post_install.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-echo "FUCK OFF"
+DSSYSTEM="dssystem-14.0.txz"
+URL="https://builds.ixsystems.com/~john/asigra/Software/DS-System/FreeBSD"
+
+fetch ${URL}/${DSSYSTEM}
+if [ "$?" != "0" ]; then
+	echo "ERROR: Failed fetching ${DSSYSTEM}"
+	exit 1
+fi
+
+tar -zxvf ${DSSYSTEM} -C /
