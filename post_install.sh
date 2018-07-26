3d981eb3a9433262ed49d9b4a6d5ae7b738e7d37
John Hixson <john@ixsystems.com>
2018-07-26 15:35:46 -0400

    Testing empty jail

--- post_install.sh
+++ post_install.sh
@@ -1,13 +1,24 @@
 #!/bin/sh
 
-DSSYSTEM="dssystem-14.0.txz"
-URL="https://builds.ixsystems.com/ix-iso/john/asigra/Software/DS-System/FreeBSD"
-URL="http://12.189.233.133/ix-iso/john/asigra/Software/DS-System/FreeBSD"
 
-fetch -v ${URL}/${DSSYSTEM}
-if [ "$?" != "0" ]; then
-	echo "ERROR: Failed fetching ${DSSYSTEM}"
+install_asigra()
+{
+	local dssystem="dssystem-14.0.txz"
+	local url="https://builds.ixsystems.com/ix-iso/john/asigra/Software/DS-System/FreeBSD"
+
+	fetch -v ${url}/${dssystem}
+	if [ "$?" != "0" ]; then
+	echo "ERROR: Failed fetching ${dssystem}"
 	exit 1
-fi
+	fi
 
-tar -zxvf ${DSSYSTEM} -C /
+	tar -zxvf ${dssystem} -C /
+}
+
+main()
+{
+	echo 'Installing Asigra'
+	#install_asigra
+}
+
+main;
