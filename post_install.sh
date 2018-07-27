933e08b472878919e68cd1e4179f96020229df5e
John Hixson <john@ixsystems.com>
2018-07-27 14:17:02 -0400

    Get asigra tarball in /root

--- post_install.sh
+++ post_install.sh
@@ -6,13 +6,14 @@ install_asigra()
 	local dssystem="dssystem-14.0.txz"
 	local url="https://builds.ixsystems.com/ix-iso/john/asigra/Software/DS-System/FreeBSD"
 
+	cd /root
 	fetch -v ${url}/${dssystem}
 	if [ "$?" != "0" ]; then
 	echo "ERROR: Failed fetching ${dssystem}"
 	exit 1
 	fi
 
-	tar -zxvf ${dssystem} -C /
+	#tar -zxvf ${dssystem} -C /
 }
 
 setup_inetd()
