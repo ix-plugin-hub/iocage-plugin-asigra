6b87d0b3c18e3a98b141b1aa7963f323ab8d9e5a
John Hixson <john@ixsystems.com>
2018-08-22 15:11:13 -0400

    quotetricks

--- post_install.sh
+++ post_install.sh
@@ -164,7 +164,7 @@ setup_nginx()
 
 	mkdir -p "${wwwpath}"
 	unzip "${dsoperator}" -d "${wwwpath}"
-	sed -i.bak -E 's|codebase="(.+)"|codebase="http://$ip:8888/asigra/"|' /usr/local/www/asigra/DSOP.jnlp
+	sed -i.bak -E "s|codebase=\"(.+)\"|codebase=\"http://$ip:8888/asigra/\"|" /usr/local/www/asigra/DSOP.jnlp
 
 	# Should fix this for IPv6 too
 	sed -i.bak -E "s/listen[[:blank:]]+80/listen $ip:8888/" /usr/local/etc/nginx/nginx.conf
