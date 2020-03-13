ed7259ed5e2cebccb2bba63d163a759da15a6862
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-03-13 11:57:41 +0500

    Fix typos

--- pre_update.sh
+++ pre_update.sh
@@ -5,10 +5,7 @@ rm -f /tmp/update_dump
 pg_dump -U pgsql -d dssystem > /tmp/update_dump
 
 echo "Stopping postgresql"
-service postgres stop
-
-echo "Deleting postgresql packages"
-pkg info | grep postgresql | awk '{print $1}' | xargs pkg delete -yf
+service postgresql stop
 
 echo "Removing postgresql data directory"
 # We do this as newer version of postgresql probably won't support the directory
