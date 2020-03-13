1cb66bf6755cc034007f7600a91f23d25a13c5fd
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-03-13 13:00:31 +0500

    Abort update if dump not made

--- pre_update.sh
+++ pre_update.sh
@@ -7,10 +7,15 @@ echo "Generating dump of postgresql"
 rm -f /tmp/update_dump
 pg_dump -U pgsql -d dssystem > /tmp/update_dump
 
+if [ $? != 0 ]; then
+	echo "Failed to take dump of existing database. Aborting update."
+	exit 1
+fi
+
 echo "Stopping postgresql"
 service postgresql stop
 
 echo "Removing postgresql data directory"
 # We do this as newer version of postgresql probably won't support the directory
 # initialised by an older version of postgresql
-rm -rf /usr/local/pgsql/data
+rm -rf /usr/local/pgsql/data/* | true
